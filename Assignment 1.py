786c504de3f8bfcaa38d3aa3a20e69d85e49b585
kaifmulla <56465694+kaifmulla@users.noreply.github.com>
2021-12-16 20:36:19 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,6 @@
+# To show the full integer value of division
+a = int(input("Enter the value of a"))
+b = int(input("Enter the value of b"))
+print(a%b)              #this will give the remainder
+print(a/b)              #this will give the value of division in decimal
+print(a//b)             #this will give the integer value or whole value of division
